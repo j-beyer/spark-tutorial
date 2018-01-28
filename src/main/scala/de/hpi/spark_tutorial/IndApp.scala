5276e877a1dd8fb88cd452ab53583771de4e63e8
Jonas Beyer <jonas.beyer@student.hpi.de>
2018-01-28 16:04:17 +0100

    Fix OOM and improve pipeline in general

--- src/main/scala/de/hpi/spark_tutorial/IndApp.scala
+++ src/main/scala/de/hpi/spark_tutorial/IndApp.scala
@@ -54,101 +54,39 @@ class IndApp {
     spark.sparkContext.setLogLevel("OFF")
 
     // Set the default number of shuffle partitions to 5 (default is 200, which is too high for local deployment)
-    spark.conf.set("spark.sql.shuffle.partitions", s"$numPartitions") //
-    spark.conf.set("spark.sql.crossJoin.enabled", "true")
+    spark.conf.set("spark.sql.shuffle.partitions", s"$numPartitions")
 
     // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
     import spark.implicits._
 
-    //
-    //    val stringTypedDS = untypedDF.map(r => r.get(0).toString) // DF to DS via map
-    //    val integerTypedDS = untypedDF.as[Int] // DF to DS via as() function that cast columns to a concrete types
-    //
-    //    // Grouping and aggregation for Datasets
-    //    val topEarners = employees
-    //      .groupByKey { case (name, age, salary, company) => company }
-    //      .mapGroups { case (key, iterator) =>
-    //          val topEarner = iterator.toList.maxBy(t => t._3) // could be problematic: Why?
-    //          (key, topEarner._1, topEarner._3)
-    //      }
-    //      .sort(desc("_3"))
-    //    topEarners.collect().foreach(t => println(t._1 + "'s top earner is " + t._2 + " with salary " + t._3))
-    //
-    //    employees.printSchema() // print schema of dataset/dataframe
-    //    topEarners.explain() // print Spark's physical query plan for this dataset/dataframe
-    //    topEarners.show() // print the content of this dataset/dataframe
-    //
-    //
-    //
-    //    persons
-    //      .map(_.name + " says hello")
-    //      .collect()
-    //      .foreach(println(_))
-    //
-    //
-    //    // Solution: broadcast variable
-    //    val bcNames = spark.sparkContext.broadcast(names)
-    //    val bcFiltered1 = employees.filter(e => bcNames.value.contains(e._1)) // a copy of names is shipped to each executor
-    //    val bcFiltered2 = employees.filter(e => !bcNames.value.contains(e._1)) // a copy of names is already present
-    //    val bcFiltered3 = employees.filter(e => bcNames.value(1).equals(e._1)) // a copy of names is already present
-    //    List(bcFiltered1, bcFiltered2, bcFiltered3).foreach(_.show(1))
-    //    bcNames.destroy() // finally, destroy the broadcast variable to release it from memory in each executor
-    //
-    //    val region = spark.read
-    //      .option("inferSchema", "true")
-    //      .option("header", "true")
-    //      .option("sep", ";")
-    //      .csv(s"$path/tpch_region.csv")
-    //
-    //    val columnNames = region.columns
-    //
-    //    val regionCells = region.rdd.map(row => row.toSeq.zip(columnNames)).collect
-    //    regionCells.foreach(println(_))
 
     val files = getListOfFiles(path)
 
+    // read all files into dataframes
     val dataFrames = files.map(file =>
       spark.read
-        .option("inferSchema", "true")
+        .option("inferSchema", "false")
         .option("header", "true")
         .option("sep", ";")
         .csv(s"$file")
     )
 
+    // extract cells from each dataframe, and union the resulting cells
     val cells = dataFrames
-      .flatMap(dataFrame => {
-        println(s"zipping for ... ${dataFrame.columns(0)}")
+      .map(dataFrame => {
         val columnNames = dataFrame.columns
         val rowCells = dataFrame
           .map(row => row.toSeq.asInstanceOf[Seq[String]])
           .flatMap(row => row.zip(columnNames))
-          .collect
+          .map(pair => pair._1 -> Seq(pair._2))
         rowCells
       })
-      .toDS
-      .map(pair => pair._1->Seq(pair._2))
-
-
-    // preaggregation, concat column title for same value
-    val preAggregatedCells = cells.groupByKey(value => value._1)
-      .reduceGroups((cell1, cell2) => (cell1._1, cell1._2 ++ cell2._2))
-      .map(_._2)
-      .map(pair => (pair._1, pair._2.distinct))
-
-    // redistribute by key
-    val repartitionedCells = preAggregatedCells.repartition(numPartitions)
-
-    repartitionedCells.show
+      .reduce(_ union _)
 
     // generate attribute sets
-    val attributeSets = repartitionedCells
-      .groupByKey(value => value._1)
-      .reduceGroups((cell1, cell2) => (cell1._1, cell1._2 ++ cell2._2))
+    val attributeSets = cells.rdd
+      .reduceByKey(_ ++ _ distinct)
       .map(_._2)
-      .map(pair => pair._2.distinct)
-      .distinct()
-
-    attributeSets.show
 
     // generate inclusion lists
     val inclusionLists = attributeSets
@@ -157,35 +95,15 @@ class IndApp {
         set.map(el => (el, (set - el).toSeq)).toSeq
       })
 
-    // redistribute by key
-    val repartitionedInclusionLists = inclusionLists.repartition(numPartitions)
-
-    repartitionedInclusionLists.show
-
     // aggregate, i.e., intersect inclusion list values
-    val aggregated = repartitionedInclusionLists
-      .groupByKey(value => value._1)
-      .reduceGroups((cell1, cell2) => (cell1._1, cell1._2.toSet.intersect(cell2._2.toSet).toSeq))
-      .map(_._2)
-
-      // remove empty sets
+    val aggregated = inclusionLists
+      .reduceByKey(_ intersect _)
       .filter(_._2.nonEmpty)
+      .sortByKey(ascending = true, 1)
 
-    val inds = aggregated
-      .map(pair => pair._1 + " < " + pair._2.mkString(", "))
-      .collect
+    // print output
+    aggregated.foreach(pair => println(s"${pair._1} < ${pair._2.mkString(", ")}"))
 
-    inds.foreach(println(_))
-
-  }
-
-  private def zipRowWithColumnNames(dataFrame: DataFrame): Array[(String, String)] = {
-    println("zipRowWithColumnNames called")
-    val columnNames = dataFrame.columns
-    val cells = dataFrame
-      .map(row => row.toSeq.asInstanceOf[Seq[String]])
-      .flatMap(row => row.zip(columnNames))
-    cells
   }
 
   private def getListOfFiles(dir: String): List[File] = {
